c56e0351265ff57c1128334595b57dff25530f10
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-28 16:03:58 -0600

    Destroy other elemenst if the User is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   after_save :update_counters
   def last_three_posts
