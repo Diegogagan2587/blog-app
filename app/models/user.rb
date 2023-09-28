6e3b60f17094c21fc3772134988593faf3b4878c
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-28 16:46:24 -0600

    Refactor Post counters: it will be defined insice Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  after_save :update_counters
   def last_three_posts
     posts.order(created_at: :desc).limit(3)
-  end
-  def update_counters
-    update(posts_counter: posts.count)
   end
 end
 
