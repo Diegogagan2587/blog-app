d2af626b2eb337d317e7ecd62a0bf314dc7f5599
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-28 15:32:44 -0600

    Update Post counter after saving the data

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,13 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :comments
   has_many :likes
+
+  after_save :update_counters
   def last_three_posts
     posts.order(created_at: :desc).limit(3)
+  end
+  def update_counters
+    update(posts_counter: posts.count)
   end
 end
 
