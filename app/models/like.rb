c1a0efcf47dbd243c96840e65f51809442f14bc8
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-28 19:21:52 -0600

    Implement Likes counter: will update countr witin Post table

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_save :update_counter_likes
+
+  def update_counter_likes
+    post.update(likes_counter: post.likes.count)
+  end
 end
