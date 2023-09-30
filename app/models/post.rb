a1f18ba968af96733df5a702f1fd8c74a9314a6b
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-29 18:53:41 -0600

    Refacator post model to pass test

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence: true, length: { maximum: 250 }
+
+  before_save :validate_comments_counter
+  before_save :validate_likes_counter
+
   after_save :update_counter_posts
   # next method will reutunr 5 most recent comments for a post
   def five_recent_comments
@@ -11,5 +16,13 @@ class Post < ApplicationRecord
 
   def update_counter_posts
     author.update(posts_counter: author.posts.count)
+  end
+
+  def validate_comments_counter
+    self.comments_counter = 0 if self.comments_counter.nil?  
+  end
+
+  def validate_likes_counter
+    self.likes_counter = 0 if self.likes_counter.nil?
   end
 end
