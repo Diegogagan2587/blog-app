f2d35f7e78a87a633e129ba192d0b64253f8775d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-29 19:02:04 -0600

    Add test for Like model

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  user = User.create(name: 'Tom', photo:'#', bio: 'Teacher from Mexico' )
+  post = Post.create(author: user, title: 'How to gain a dollar', text: 'Get a job')
+  subject { Like.new(user: user, post: post) }
+  before { subject.save }
+
+  it 'should be valid' do
+    expect(subject).to be_valid
+  end
+
+  it 'should not be valid without a user' do
+    subject.user = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should not be valid without a post' do
+    subject.post = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should update likes_counter after save' do
+    expect(post.likes_counter).to eq(1)
+  end
+
+  it 'should update likes_counter after destroy' do
+    subject.destroy
+    expect(post.likes_counter).to eq(0)
+  end
+end
