c71b7c6f469853cd02a19a60de6e879f49dfe4ef
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-06 10:52:21 -0600

    Fix test case to correctly test show action in PostsController

--- spec/requests/posts/show_spec.rb
+++ spec/requests/posts/show_spec.rb
@@ -4,14 +4,23 @@ RSpec.describe PostsController, type: :controller do
     before { Rails.cache.clear }
     before {
         @user = User.new(name: 'diego', photo: '@', bio: 'bio')
+        @post = Post.new(author: @user, text: 'text', title: 'title')
+        @post.save
         @user.save
     }
     #next line will allow to check if the placeholder text is present
     render_views
 
-    #test if status was correct
-    it 'returns a 200 OK statud' do
-      get :index, params: { user_id: @user.id}
-      expect(response.status).to eq(200)
+    describe 'GET #show' do
+      #test if status was correct
+        it 'returns a 200 OK status' do
+        #next line simulate get: /users/@user.id/posts/post.id
+        get :show, params: { user_id: @user.id, id: @post.id }
+        expect(response.status).to eq(200)
+        end
+
+        #test if correct template was rendered
+        it 'renders the show template' do
+        end
     end
 end
