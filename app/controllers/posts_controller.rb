e52abf07cb6fe12da4375b67a9574d8f6de51114
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-07 00:36:37 -0600

    Implemente show action for post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,12 @@ class PostsController < ApplicationController
   end
 
   def show;
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+    #we get the index of the current post
+    @current_post_number = @user.posts.index(@post)+1
+    #we get count of comments in the post
+    @count_comments = @post.comments.count
+    @count_likes = @post.likes.count
   end
 end
