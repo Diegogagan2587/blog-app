7cc0ebd2975d151c9fd9f93338a242142a37a5b3
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-07 03:36:35 -0600

    Fix linting errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
-  def index; 
+  def index
     @user = User.find(params[:user_id])
   end
 
-  def show;
+  def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
-    #we get the index of the current post
-    @current_post_number = @user.posts.index(@post)+1
-    #we get count of comments in the post
+    # we get the index of the current post
+    @current_post_number = @user.posts.index(@post) + 1
+    # we get count of comments in the post
     @count_comments = @post.comments.count
     @count_likes = @post.likes.count
   end
