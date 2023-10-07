d1a77b346490fa2b66474435564ef9c5521e90c2
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-07 10:07:48 -0600

    Make comments availabe as instance variable

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
+    @comments = @post.comments
     # we get the index of the current post
     @current_post_number = @user.posts.index(@post) + 1
     # we get count of comments in the post
