6e4add14cebc330e2cec483f42d8d9de485086f1
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-07 21:30:39 -0600

    Set route for like button

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/', to: redirect('/users')
   resources :users, only: [:index, :show]  do
     resources :posts, only: [:index, :show, :new, :create] do 
+      post 'add_like', on: :member
       resources :comments, only: [:new, :create]
     end
   end
