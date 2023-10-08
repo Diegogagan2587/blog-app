2b8b3d3cdb6d66ae8708af400b9cefca21555f16
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-07 18:41:19 -0600

    Create new route for the add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get '/', to: redirect('/users')
   resources :users, only: [:index, :show]  do
-    resources :posts, only: [:index, :show, :new, :create]
+    resources :posts, only: [:index, :show, :new, :create] do 
+      resources :comments, only: [:new, :create]
+    end
   end
 end
 
