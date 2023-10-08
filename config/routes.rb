6405145b59932480650479cc9e3a96db64a184c3
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-07 22:23:02 -0600

    Add routing for removinb a like

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]  do
     resources :posts, only: [:index, :show, :new, :create] do 
       post 'add_like', on: :member
+      delete 'delete_like', on: :member
       resources :comments, only: [:new, :create]
     end
   end
