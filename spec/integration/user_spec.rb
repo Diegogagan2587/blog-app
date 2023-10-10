429b37f10c4479a1ce0fa63bc4f3063abedf192d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-10 12:00:10 -0600

    Add test case for user index page: show all users names

--- spec/integration/user_spec.rb
+++ spec/integration/user_spec.rb
@@ -3,19 +3,21 @@ require 'rails_helper'
 RSpec.describe 'User', type: :system do
   # lets test user index page first.
   describe 'User index page' do
-    before(:each) { Rails.cache.clear }
     before(:each) do
-      @user_one = User.create!(name: 'Tom', photo: @img, bio: 'Teacher from Mexico, living in Japan')
+      Rails.cache.clear
+      @img = 'icons/icons8-user-60.png'
+      @user_one = User.create!(name: 'Mike', photo: @img, bio: 'Teacher from Mexico, living in Japan')
       @user_two = User.create!(name: 'Jerry', photo: @img, bio: 'Student from Japan')
-      puts 2
     end
 
     # check if we can see the user name of all users
     it "shows all users's username" do
       visit users_path
 
-      expect(page).to have_content('Tom')
+      expect(page).to have_content('Mike')
       expect(page).to have_content('Jerry')
     end
+
+   
   end
 end
