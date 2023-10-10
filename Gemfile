721df2e71b8d9c9c96d1f9dda0b49c17499b4bf3
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-10 01:51:22 -0600

    Add first working integration test

--- Gemfile
+++ Gemfile
@@ -51,6 +51,10 @@ gem 'bootsnap', require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+    gem 'selenium-webdriver'
+  gem 'webdrivers'
+    gem 'rspec-rails'
+    gem 'capybara'
 end
 
 group :development do
@@ -66,15 +70,14 @@ end
 
 # group :test do
 #   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
-#   
-#   
+#
+#
 # end
 group :test do
+  
   gem 'rails-controller-testing'
-  gem 'rspec-rails'
-  gem 'capybara'
-  gem 'selenium-webdriver'
-  gem 'webdrivers'
+
+
 end
 
 # rubocop
---
+++ spec/integration/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'User', type: :system do
+  # lets test user index page first.
+  describe 'User index page' do
+    before(:each) { Rails.cache.clear }
+    before(:each) do
+      img = 'icons/icons8-user-60.png'
+      @user_one = User.create!(name: 'Tom', photo: @img, bio: 'Teacher from Mexico, living in Japan')
+      @user_two = User.create!(name: 'Jerry', photo: @img, bio: 'Student from Japan')
+      puts 2
+    end
+
+    # check if we can see the user name of all users
+    it "shows all users's username" do
+      
+      visit users_path 
+
+      expect(page).to have_content('Tom')
+      expect(page).to have_content('Jerry')
+      
+    end
+  end
+end
