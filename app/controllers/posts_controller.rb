e7c148710cf46fa2b5a25141fd0355c091e19e6b
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-11 15:23:56 -0600

    Optimize request to avoid N+1 problems

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.order(created_at: :desc).page(params[:page]).per(5)
+    @posts = @user.posts.includes(:author).order(created_at: :desc).page(params[:page]).per(5)
   end
 
   def add_like
