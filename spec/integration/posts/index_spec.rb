b317ae2fb49b2bdf2ed448ccb3dc0e92aae25586
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-10 21:59:35 -0600

    Add test case Posts@index: should show the number of comments the post has

--- spec/integration/posts/index_spec.rb
+++ spec/integration/posts/index_spec.rb
@@ -8,12 +8,14 @@ RSpec.describe 'Post', type: :system do
         
         @img = 'icons/icons8-user-60.png'
         @user_one = User.create!(name: 'Mike', photo: @img, bio: 'Teacher from Mexico, living in Japan')
+        @user_two = User.create!(name: 'John', photo: @img, bio: 'Teacher from USA, living in Japan')
         
         @post_one = Post.create!(author: @user_one, title: 'Mike post 1', text: 'Mike post 1 text')
         @post_two = Post.create!(author: @user_one, title: 'Mike post 2', text: 'Mike post 2 text')
         @post_three = Post.create!(author: @user_one, title: 'Mike post 3', text: 'Mike post 3 text')
         @post_four = Post.create!(author: @user_one, title: 'Mike post 4', text: 'Mike post 4 text')
         @post_five = Post.create!(author: @user_one, title: 'Mike post 5', text: 'Mike post 5 text')
+        @post_six = Post.create!(author: @user_two, title: 'John post 1', text: 'John post 1 text')
 
         @comment_one = Comment.create!(post: @post_one, author: @user_one, text: 'Mike post 1 comment 1')
 
@@ -55,6 +57,12 @@ RSpec.describe 'Post', type: :system do
         it "should show the first comment of the post" do
             visit user_posts_path(@user_one)
             expect(page).to have_content('Mike post 1 comment 1')
+        end
+
+        it "should show the number of comments the post has" do
+            visit user_posts_path(@user_one)
+            expect(page).to have_content('Comments: 1')
+            expect(page).to have_content('Comments: 0')
         end
     end
 end
