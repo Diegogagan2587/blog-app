8665e1aa5342ad971e0aa3e8718d745128135cb7
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-11 20:06:10 -0600

    Add test case for posts#index: shoud show the number of commetns the post has

--- spec/integration/posts /index_spec.rb
+++ spec/integration/posts /index_spec.rb
@@ -61,5 +61,11 @@ RSpec.describe 'Post', type: :system do
       expect(page).to have_content('Mike post 1 comment 1')
     end
 
+    it 'should show the number of comments the post has' do
+      visit user_posts_path(@user_one)
+      expect(page).to have_content('Comments: 1')
+      expect(page).to have_content('Comments: 0')
+    end
+
   end
 end
