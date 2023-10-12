358bc9067f8885af093004f86baebe9798129c69
rudicarrilloypr <rudicarrilloypr@gmail.com>
2023-10-12 16:54:11 -0600

    Permit :name to create a new user in devise registrations_controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+# Create registration controller to override devise default behaviour
+class RegistrationsController < Devise::RegistrationsController
+  # we need to permig name and bio params
+  before_action :configure_permitted_parameters
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+  end
+end
