9105dd3a7ce7dfa6685990eb764544a164174738
Diegogagan2587 <diego27796gagan@gmail.com>
2023-10-12 17:05:46 -0600

    Fix Error: when users/sing_out now will sing out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,18 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    # redirect to log_out acction if id = sing_out
+    if params[:id] == 'sign_out'
+      log_out
+    else
+      @user = User.find(params[:id])
+    end
+  end
+
+  # def a method to handle log_out
+  def log_out
+    # sign_out is a method from devise
+    sign_out current_user
+    redirect_to new_user_session_path
   end
 end
