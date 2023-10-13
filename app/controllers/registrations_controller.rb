a6bb22fecfb39d6d24693d0d75445b6a2465a02f
Diegogagan2587 <diego27796gagan@gmail.com>
2023-10-12 20:54:31 -0600

    Allow name modification and bio

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,5 +7,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name bio])
   end
 end
